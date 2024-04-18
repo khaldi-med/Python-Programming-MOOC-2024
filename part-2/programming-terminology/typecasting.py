0ee65299d84a796d06184af14e79dced97033e2b
mohamed <khaldimohamed82@gmail.com>
2024-04-18 19:04:04 +0100

    Notes from my learning journey

---
+++ part-2/programming-terminology/typecasting.py
@@ -0,0 +1,6 @@
+number = float(input("Please type in a number: "))
+
+if number < 0:
+    input("Please type in a number great than 0: ")
+print(f"Integer part: {int(number)}")
+print(f"Decimal part: {int(number) - number}")
