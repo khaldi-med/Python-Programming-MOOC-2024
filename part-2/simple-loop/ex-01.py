6bd98214c4ed1121251b460d650b02a48b20aeb1
mohamed <khaldimohamed82@gmail.com>
2024-05-08 14:51:39 +0100

    My learning journey practice

---
+++ part-2/simple-loop/ex-01.py
@@ -0,0 +1,14 @@
+# Shall we continue?
+
+print("hi")
+
+while True:
+    userInput = input("Shall we continue? ")
+
+    if userInput != "no":
+        print("hi")
+    else:
+        print("okay then")
+        break
+    
+
