81bacc4ef98cbd4fdcfbe0855d92dd165063f156
mohamed <khaldimohamed82@gmail.com>
2024-05-11 00:46:22 +0100

    My learning journey practice

--- part-2/simple-loop/ex-05.py
+++ part-2/simple-loop/ex-05.py
@@ -1,17 +1,19 @@
 # PIN and number of attempts
 
-while True:
-pin = input("PIN: ")
 pinNum = 4321
 triedNum = 0
 
-if pin != pinNum:
-    triedNum +=1
-    if pin == pinNum:
+while True:
+    pin = int(input("PIN: "))
+    triedNum += 1
+
+    if pin != pinNum:
+        print("Wrong")
+    elif pin == pinNum:
         if triedNum == 1:
-            print("Correct! It only took you one single attempt!")
-            break
+          print("Correct! It only took you one single attempt!")
+          break
         else:
-            print(f"Correct! It took you {triedNum} attempts")
-            break
+          print(f"Correct! It took you {triedNum} attempts")
+          break
 
