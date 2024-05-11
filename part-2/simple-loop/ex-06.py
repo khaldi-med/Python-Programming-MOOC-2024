558cac66f15afc9a75055f4f5b32c00284b1912c
mohamed <khaldimohamed82@gmail.com>
2024-05-11 01:22:41 +0100

    My learning journey practice

---
+++ part-2/simple-loop/ex-06.py
@@ -0,0 +1,25 @@
+# The next leap year
+
+year = int(input("Please type in a year: "))
+
+while True:
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                nextYear += 1
+                newLeap = year + 1
+                if nextYear == 2:
+                    print("The next leap year after {year} is {newLeap}")
+                    break
+    else:
+        year += 1
+    
+                
+              
+
+
+
+
+
+
+
