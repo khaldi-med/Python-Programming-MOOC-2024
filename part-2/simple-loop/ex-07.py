4fb531fa16d0ec8bb8f4acd9afa231eb06ecdeb9
mohamed <khaldimohamed82@gmail.com>
2024-05-17 16:52:02 +0100

    My learning journey practice

---
+++ part-2/simple-loop/ex-07.py
@@ -0,0 +1,15 @@
+# Write your solution here
+story = ""
+last_word = ""
+while True:
+    word = input("Please type in a word: ")
+
+                if word == "end":
+                    print(story)
+                    break
+                if word == last_word:
+                    print(story)
+                    break
+
+story += word + " "
+last_word = word
