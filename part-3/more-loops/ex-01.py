ebc4c6d9358c75c7773e68a3291d69252e46ef04
mohamed <khaldimohamed82@gmail.com>
2024-06-13 18:01:17 +0100

    My learning journey practice

--- part-3/more-loops/ex-01.py
+++ part-3/more-loops/ex-01.py
@@ -1,6 +1,16 @@
 # Multiplication
 
-number = input("Please type in a number: ")
-
-index = 0
 while True:
+    number = int(input("Please type in a number: "))
+    if number > 0:
+        break
+
+leftNum = 1
+while leftNum <= number:
+    rightNum = 1
+    while rightNum <= number:
+        product = leftNum * rightNum
+        print(f"{leftNum} x {rightNum} = {product}")
+        rightNum += 1
+    leftNum += 1
+
