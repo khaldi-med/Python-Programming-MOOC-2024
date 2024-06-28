9f7874053b3ca65f7c54d0361256aa00ac2afc68
mohamed <khaldimohamed82@gmail.com>
2024-06-28 15:56:12 +0100

    My learning journey practice

--- part-3/more-loops/ex-03.py
+++ part-3/more-loops/ex-03.py
@@ -1,15 +1,16 @@
 # Factorial
 
-number = int(input("Please type in a number: "))
+while True:
+    number = int(input("Please type in a number: "))
+    factorial = 1
+    i = 1
 
-factorial = 1
-i = 1
-while number > 0:
+    if number <= 0:
+        print("Thanks and bye!")
+        break
     while i <= number:
         factorial *= i
         i += 1
         continue
     print(f"The factorial of the number {number} is {factorial}")
-    break
-if number <= 0:
-    print("Thanks and bye!")
+
