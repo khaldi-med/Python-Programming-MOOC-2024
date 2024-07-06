0160c7576c4ee06aaf5bcddd423b970ef590be64
mohamed <khaldimohamed82@gmail.com>
2024-07-06 17:04:17 +0100

    Expanding my Skillset, Embracing Continuous Learning

---
+++ part-3/defining-functions/ex-02.py
@@ -0,0 +1,11 @@
+#The first character
+
+def first_character(text):
+    print(text[0])
+
+first_character('python')
+first_character('yellow')
+first_character('tomorrow')
+first_character('heliotrope')
+first_character('open')
+first_character('night')
