508b4325af1da257c97d857d383d6c2de5ccde9f
mohamed <khaldimohamed82@gmail.com>
2024-07-06 18:16:46 +0100

    Expanding my Skillset, Embracing Continuous Learning

---
+++ part-3/defining-functions/ex-04.py
@@ -0,0 +1,14 @@
+# Print many times
+
+def print_many_times(text, times):
+    i = 0
+    while i < times:
+       print(text)
+       i += 1
+
+text = "All Pythons, except one, grow up"
+times = 3
+
+print_many_times(text, times)
+
+print_many_times("python", 5)
