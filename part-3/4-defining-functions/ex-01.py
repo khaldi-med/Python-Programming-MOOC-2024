6140a366d6b0d0aaa06eb4b6ce51d7c936d643e7
mohamed <khaldimohamed82@gmail.com>
2024-09-10 14:19:21 +0100

    Expanding my Skillset, Embracing Continuous Learning

---
+++ part-3/4-defining-functions/ex-01.py
@@ -0,0 +1,13 @@
+# Seven Brothers
+
+
+def seven_brothers():
+    names = ["Aapo", "Eero", "Juhani", "Lauri", "Simeoni", "Timo", "Tuomas"]
+    i = 0
+
+    while i < len(names):
+        print(names[i])
+        i += 1
+
+
+seven_brothers()
